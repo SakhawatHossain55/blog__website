78a38c5dc4881598ea386884bfaa4f26e7f7fd49
SakhawatHossain55 <sakhawatHossain7969@gmail.com>
2021-09-10 08:25:30 +0600

    [ADD] post or sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
+import TopBar from "./components/topBar/TopBar";
 import Home from "./pages/home/Home";
-import TopBar from "./topBar/TopBar";
+import Single from "./pages/single/Single";
 
 function App() {
   return (
     <>
     <TopBar />
-    <Home />
+    <Single />
     </>
   );
 }
