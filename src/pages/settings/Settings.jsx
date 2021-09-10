5885bf89d744d5b944996265fa15a67e49a2adda
SakhawatHossain55 <sakhawatHossain7969@gmail.com>
2021-09-10 23:47:25 +0600

    [ADD] sum

--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -1,7 +1,46 @@
-import Sidebar from "../../components/sidebar/Sidebar";
 import "./settings.css";
+import Sidebar from "../../components/sidebar/Sidebar";
+import { useContext, useState } from "react";
+import { Context } from "../../context/Context";
+import axios from "axios";
 
 export default function Settings() {
+  const [file, setFile] = useState(null);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  const { user, dispatch } = useContext(Context);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    dispatch({ type: "UPDATE_START" });
+    const updatedUser = {
+      userId: user._id,
+      username,
+      email,
+      password,
+    };
+    if (file) {
+      const data = new FormData();
+      const filename = Date.now() + file.name;
+      data.append("name", filename);
+      data.append("file", file);
+      updatedUser.profilePic = filename;
+      try {
+        await axios.post("/upload", data);
+      } catch (err) {}
+    }
+    try {
+      const res = await axios.put("/users/" + user._id, updatedUser);
+      setSuccess(true);
+      dispatch({ type: "UPDATE_SUCCESS", payload: res.data });
+    } catch (err) {
+      dispatch({ type: "UPDATE_FAILURE" });
+    }
+  };
+
   return (
     <div className="settings">
       <div className="settingsWrapper">
@@ -12,10 +51,7 @@ export default function Settings() {
         <form className="settingsForm">
           <label>Profile Picture</label>
           <div className="settingsPP">
-            <img
-              src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-              alt=""
-            />
+            <img src={user.profilePic} alt="" />
             <label htmlFor="fileInput">
               <i className="settingsPPIcon far fa-user-circle"></i>{" "}
             </label>
@@ -27,10 +63,22 @@ export default function Settings() {
             />
           </div>
           <label>Username</label>
-          <input type="text" placeholder="Safak" name="name" />
+          <input
+            type="text"
+            placeholder={user.username}
+            onChange={(e) => setUsername(e.target.value)}
+            name="name"
+          />
           <label>Email</label>
-          <input type="email" placeholder="safak@gmail.com" name="email" />
-          <label>Password</label>
+          <input
+            type="email"
+            placeholder={user.email}
+            onChange={(e) => setEmail(e.target.value)}
+            name="email"
+          />
+          <label type="password" onChange={(e) => setPassword(e.target.value)}>
+            Password
+          </label>
           <input type="password" placeholder="Password" name="password" />
           <button className="settingsSubmitButton" type="submit">
             Update
