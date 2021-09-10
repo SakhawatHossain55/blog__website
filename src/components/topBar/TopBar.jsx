acf4c375f33fd77759e8fac8ec74001db62bf5d9
SakhawatHossain55 <sakhawatHossain7969@gmail.com>
2021-09-10 10:57:19 +0600

    [FAINAL]

--- src/components/topBar/TopBar.jsx
+++ src/components/topBar/TopBar.jsx
@@ -1,26 +1,57 @@
+import { Link } from "react-router-dom";
 import "./topBar.css";
 
 export default function TopBar() {
+  const user = false;
   return (
     <div className="top">
       <div className="topLeft">
         <i className="topIcon fab fa-facebook-square"></i>
-        <i className="topIcon fab fa-twitter-square"></i>
-        <i className="topIcon fab fa-pinterest-square"></i>
         <i className="topIcon fab fa-instagram-square"></i>
+        <i className="topIcon fab fa-pinterest-square"></i>
+        <i className="topIcon fab fa-twitter-square"></i>
       </div>
       <div className="topCenter">
-          <ul className="topList">
-              <ul className="topListItem">HOME</ul>
-              <ul className="topListItem">ABOUT</ul>
-              <ul className="topListItem">CONTACT</ul>
-              <ul className="topListItem">WRITE</ul>
-              <ul className="topListItem">LOGOUT</ul>
-          </ul>
+        <ul className="topList">
+          <li className="topListItem">
+            <Link className="link" to="/">
+              HOME
+            </Link>
+          </li>
+          <li className="topListItem">ABOUT</li>
+          <li className="topListItem">CONTACT</li>
+          <li className="topListItem">
+            <Link className="link" to="/write">
+              WRITE
+            </Link>
+          </li>
+          {user && <li className="topListItem">LOGOUT</li>}
+        </ul>
       </div>
       <div className="topRight">
-          <img className="topImg" src="https://i.ibb.co/tcrCy4j/Shakib.jpg" alt="" />
-          <i className="topSearchIcon fas fa-search"></i>
+        {user ? (
+          <Link className="link" to="/settings">
+            <img
+              className="topImg"
+              src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+              alt=""
+            />
+          </Link>
+        ) : (
+          <ul className="topList">
+            <li className="topListItem">
+              <Link className="link" to="/login">
+                LOGIN
+              </Link>
+            </li>
+            <li className="topListItem">
+              <Link className="link" to="/register">
+                REGISTER
+              </Link>
+            </li>
+          </ul>
+        )}
+        <i className="topSearchIcon fas fa-search"></i>
       </div>
     </div>
   );
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,19 +1,36 @@
+import { Link } from "react-router-dom";
 import "./register.css";
 
 export default function Register() {
   return (
     <div className="register">
-        <span className="registerTitle">Register</span>
+      <span className="registerTitle">Register</span>
       <form className="registerForm">
         <label>Username</label>
-        <input type="text" className="registerInput" placeholder="Enter your user name..." />
+        <input
+          type="text"
+          className="registerInput"
+          placeholder="Enter your user name..."
+        />
         <label>Email</label>
-        <input type="text" className="registerInput" placeholder="Enter your email..." />
+        <input
+          type="text"
+          className="registerInput"
+          placeholder="Enter your email..."
+        />
         <label>Password</label>
-        <input type="password" className="registerInput" placeholder="Enter your password..." />
+        <input
+          type="password"
+          className="registerInput"
+          placeholder="Enter your password..."
+        />
         <button className="registerButton">Register</button>
       </form>
-      <button className="registerLoginButton">Login</button>
+      <button className="registerLoginButton">
+        <Link className="link" to="/login">
+          Login
+        </Link>
+      </button>
     </div>
   );
 }
