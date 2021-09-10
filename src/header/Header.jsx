c7a15751eec457893022894ff3f37eede195dde5
SakhawatHossain55 <sakhawatHossain7969@gmail.com>
2021-09-10 06:48:23 +0600

    [ADD] header

---
+++ src/header/Header.jsx
@@ -0,0 +1,14 @@
+import './header.css'
+
+export default function Header() {
+    return (
+        <div className="header">
+            <div className="headerTitle">
+                <span className="headerTitleSm">React & Node</span>
+                <span className="headerTitleLg">Blog</span>
+
+            </div>
+            <img src="https://i.ibb.co/2KjTkP5/rana-sawalha-Ihu-HLIx-S-Tk-unsplash.jpg" alt="" className="headerImg" />
+        </div>
+    )
+}
