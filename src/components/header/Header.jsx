a98ffa5abff0c39d78e7f53a09f21ce634ccb6c9
SakhawatHossain55 <sakhawatHossain7969@gmail.com>
2021-09-10 19:33:43 +0600

    [ADD] valedetion login

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,7 +8,7 @@ export default function Header() {
                 <span className="headerTitleLg">Blog</span>
 
             </div>
-            <img src="https://i.ibb.co/2KjTkP5/rana-sawalha-Ihu-HLIx-S-Tk-unsplash.jpg" alt="" className="headerImg" />
+            <img src="https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" className="headerImg" />
         </div>
     )
 }
